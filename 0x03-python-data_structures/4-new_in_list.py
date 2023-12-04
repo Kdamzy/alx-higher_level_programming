7d275c00a3edd3335501b674e79a4a8c709293c2
kdamzy <kolajidedamilare@gmail.com>
2023-12-04 11:21:47 -0800

    function that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,6 @@ def new_in_list(my_list, idx, element):
     if (idx < 0) or (idx > (len(my_list)-1)):
         return my_list
 
-    dup = [i for i in my_list]
+    dup = [x for x in my_list]
     dup[idx] = element
-
     return copy

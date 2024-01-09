ba8a29d82b53fe5454e84eb2480c21b6227b32cd
kdamzy <kolajidedamilare@gmail.com>
2024-01-09 11:48:57 -0800

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""JSON string"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object"""
+
+    return json.dumps(my_obj)

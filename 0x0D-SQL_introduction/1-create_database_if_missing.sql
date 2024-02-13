5105024ec6217cfdf1b2df5e98eb9808355c0884
Kdamzy <kolajidedamilare@gmail.com>
2024-02-13 16:20:09 +0100

    script the create database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- script to creates the database hbtn_0c_0 in MySQL server
+--drop the database if it already exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+--creates the database
+CREATE DATABASE hbtn_0c_0;

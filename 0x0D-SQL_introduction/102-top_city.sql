952bf53c2e24d5d4928f779e0293a5b9b4805dbf
Kdamzy <kolajidedamilare@gmail.com>
2024-02-13 20:23:32 +0100

    top hree cities

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- script that display the top 3 cities temperature 
 SELECT city, avg(value) AS avg_temp FROM temperatures 
-WHERE month IN ('July', 'August')
-GROUP BY city 
-ORDER BY avg_temp DESC
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;

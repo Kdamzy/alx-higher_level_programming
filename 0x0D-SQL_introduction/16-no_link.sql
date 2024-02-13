52d16c023dc3919a7be043fb49955e76a462649a
Kdamzy <kolajidedamilare@gmail.com>
2024-02-13 19:55:45 +0100

    list all records

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
---script that list all record of the second_table
+-- script that list all record of the second_table
 SELECT score, name FROM second_table HAVING name IS NOT NULL ORDER BY score DESC;

da64c4ef895e1fd45600a1b36c27e990d695f1e8
Kdamzy <kolajidedamilare@gmail.com>
2024-02-15 01:56:12 +0100

    list all priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- sript that list all priviledges of the MySQL users.
-SELECT * FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

642f17d8905d7cca5d73af64ae2d20e77795e759
Kdamzy <kolajidedamilare@gmail.com>
2024-02-15 03:08:34 +0100

    list all comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
--- cript that lists all Comedy shows in the database hbtn_0d_tvshows
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT title
 FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 GROUP BY title
-ORDER BY tv_shows.title ASC;
+ORDER BY title ASC;

139890d3a519c3e4c7c7a7ce56c3cc9af58a9a3d
Kdamzy <kolajidedamilare@gmail.com>
2024-03-11 16:15:23 +0100

    scriot that print three line of string

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* script that print 3 line of strings */
+const aVar = 'C is fun';
+const bVar = 'Python is cool';
+const cVar = 'JavaScript is amazing';
+
+console.log(aVar);
+console.log(bVar);
+console.log(cVar);

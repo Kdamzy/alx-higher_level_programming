714094cf208bc026ba5944d85b86945730b8ed0a
Kdamzy <kolajidedamilare@gmail.com>
2024-03-11 20:34:51 +0100

    updated script

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,17 +2,16 @@
 
 /* updated script */
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
-    this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);

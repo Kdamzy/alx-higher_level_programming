5b2e0f423acd822db7a3d11c2fe13ebeabc756c8
Kdamzy <kolajidedamilare@gmail.com>
2024-03-11 18:41:25 +0100

    script that prints the addiion of two intergrs

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/* script that prints the addition of two numbers */
+function add (a, b) {
+  const result = a + b;
+  return result;
+}
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!isNaN(arg1) && !isNaN(arg2)) {
+  console.log(add(arg1, arg2));
+} else {
+  console.log('Missing integer');
+}

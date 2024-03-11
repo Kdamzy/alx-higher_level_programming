24bbb5bd6dacc1b634298ec49cca5545fcd57383
Kdamzy <kolajidedamilare@gmail.com>
2024-03-11 17:36:36 +0100

    script that print 3 line of strings usingarray of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* script that print 3 line of strings using array of string and a loop */
+const Var = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let a = 0; a < Var.length; a++) {
+  console.log(Var[a]);
+}

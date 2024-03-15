45a95d7ffe624b1cf8b899c456bdf2bf35cc16dd
Kdamzy <kolajidedamilare@gmail.com>
2024-03-15 12:36:01 +0100

    script that list all states with a specific name from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    ad = """SELECT * FROM states WHERE name 
-    LIKE BINARY 'N%' ORDER BY states.id"""
+    ad = """SELECT * FROM states WHERE name LIKE BINARY
+    'N%' ORDER BY states.id"""
     c.execute(ad)
     rows = c.fetchall()
     for row in rows:

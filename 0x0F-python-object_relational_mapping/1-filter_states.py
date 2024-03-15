46d0ba7212f6fcc163f9e0947ad24df0962e1160
Kdamzy <kolajidedamilare@gmail.com>
2024-03-15 12:34:30 +0100

    script that list all states with a specific name from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Script that lists all states with a name starting with N from the database """
+""" Script that lists all states with a name
+starting with N from the database """
 import MySQLdb
 import sys
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    ad = """SELECT * FROM states WHERE name
+    ad = """SELECT * FROM states WHERE name 
     LIKE BINARY 'N%' ORDER BY states.id"""
     c.execute(ad)
     rows = c.fetchall()

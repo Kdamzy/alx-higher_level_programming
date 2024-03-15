e8b133bde536ecf18c1929586439417ec99fff97
Kdamzy <kolajidedamilare@gmail.com>
2024-03-15 13:27:19 +0100

    script that takes in an argument and display all value in the state

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
+    match = sys.argv[4]
     ad = "SELECT * FROM states WHERE name LIKE %s"
-    c.execute(ad,(sys.argv[4]))
+    c.execute(ad, (match, ))
     rows = c.fetchall()
     for row in rows:
         print(row)

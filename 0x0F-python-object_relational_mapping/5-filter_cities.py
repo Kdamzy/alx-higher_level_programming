92d001c76032a2dd3fa3ecf0f2ec6d3ceee79d94
Kdamzy <kolajidedamilare@gmail.com>
2024-03-15 13:43:23 +0100

    script that takes in name of state as an arument and lit all the cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
-    ad = """SELECT cities.name FROM cities INNER JOIN states 
-            ON states.id=cities.state_id WHERE states.name=%s"""
+    ad = """SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s"""
     c.execute(ad, (sys.argv[4], ))
     rows = c.fetchall()
     tmp = list(row[0] for row in rows)

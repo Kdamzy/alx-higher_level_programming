6fafa704126593cf55a94f4d6c87973c0262c202
Kdamzy <kolajidedamilare@gmail.com>
2024-03-16 08:42:23 +0100

    script that lista ll state object from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
     
+
 if __name__ == "__main__":
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}"
     db_url = db_url.format(sys.argv[1], sys.argv[2], sys.argv[3])

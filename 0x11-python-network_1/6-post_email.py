033cf62afc0e763c3d0d42458b50f373868ec907
Kdamzy <kolajidedamilare@gmail.com>
2024-03-28 14:04:49 +0100

    script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,9 @@ script that takes in a URL and an email address
 sends a POST request to the passed URL with the email as
 a parameter and finally displays the body of the response
 """
-
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]

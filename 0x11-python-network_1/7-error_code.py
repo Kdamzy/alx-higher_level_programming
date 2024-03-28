fdd5fed9817c55d1ac70d51ea80524b704c51c38
Kdamzy <kolajidedamilare@gmail.com>
2024-03-28 14:11:53 +0100

    script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- script that takes in a letter and sends a POST request to
- http://0.0.0.0:5000/search_user with the letter as a parameter
+script that takes in a URL, sends a request to the URL
+and displays the body of the response
 """
 
 import sys

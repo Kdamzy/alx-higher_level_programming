525cef94f45cbf983dd6eb816e805cea858e4840
Mikaelao <kolajidedamilare@gmail.com>
2024-05-02 21:18:01 +0100

    updates the text color to red when user clicks on the icon

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,4 +1,5 @@
-// add the class red element when user click on tag DIV#red_header
+// add the class red element when user click on tag
+// DIV#red_header
 $('DIV#red_header').click(function () {
     $('HEADER').addClass('red');
   });
